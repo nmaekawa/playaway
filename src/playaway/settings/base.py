9328636fa34c03c43928e3f055142d6b8bebda96
nmaekawa <nmaekawa@g.harvard.edu>
2020-03-19 15:15:18 -0400

    django debug configurable via env-var

--- src/playaway/settings/base.py
+++ src/playaway/settings/base.py
@@ -24,7 +24,9 @@ PROJECT_NAME = 'playaway'
 SECRET_KEY = os.environ.get('PLAYAWAY_DJANGO_SECRET_KEY', 'CHANGE_ME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+debug = os.environ.get('DEBUG', 'false')
+DEBUG = debug.lower() == 'true'
+
 
 ALLOWED_HOSTS = ['localhost',  '127.0.0.1']
 allowed_hosts_other = os.environ.get('PLAYAWAY_ALLOWED_HOSTS', '')
