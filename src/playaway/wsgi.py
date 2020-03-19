d8025a32110563c7bb568b71c1abc1429cd696a4
nmaekawa <nmaekawa@g.harvard.edu>
2020-03-19 14:59:56 -0400

    adds dotenv to wsgi.py

--- src/playaway/wsgi.py
+++ src/playaway/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+from dotenv import load_dotenv
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playaway.settings')
+# if dotenv file, load it
+dotenv_path = None
+if 'PLAYAWAY_DOTENV_PATH' in os.environ:
+    dotenv_path = os.environ['PLAYAWAY_DOTENV_PATH']
+elif os.path.exists(os.path.join('playaway', 'settings', '.env')):
+    dotenv_path = os.path.join('playaway', 'settings', '.env')
+if dotenv_path:
+    load_dotenv(dotenv_path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playaway.settings.base")
 
 application = get_wsgi_application()
